d17c56c2e10089c5e71287384804ccd5c3d0c93e
theflippantfox <theflippantfox@gmail.com>
2025-01-03 08:48:26 +0000

    added: print snake, grow snake and run game functions

--- main.cpp
+++ main.cpp
@@ -10,17 +10,56 @@ struct Snake {
   std::vector<Cords> body;
 } snake;
 
-void print_board() {}
+Cords food;
+bool isFood = 0;
 
-void print_snake() {
-  if (snake.body.size()) {
-    std::cout << "Body!!!";
-  } else {
-    std::cout << "No Body :(";
+void gotoCords(int x, int y) {}
+
+void growSnake(int x, int y) {
+  Cords bodyitem;
+  bodyitem.x = x;
+  bodyitem.y = y;
+  snake.body.push_back(bodyitem);
+}
+
+void eatFood() {
+  if (!isFood) {
+    return;
+  }
+
+  if (snake.head.x == food.x && snake.head.y == food.y) {
+    growSnake(food.x, food.y);
   }
 }
 
+void printBoard() {}
+
+void printSnake() {
+  gotoCords(snake.head.x, snake.head.y); // Goto the cordinates of snake head
+  std::cout << "@";                      // Print @ for head
+
+  // If snake does not have any body then return
+  if (!snake.body.size()) {
+    std::cout << "No Body :(";
+    return;
+  }
+
+  // If snake has a body then print *
+  for (int i = 0; i < snake.body.size(); i++) {
+    gotoCords(snake.body[i].x, snake.body[i].y);
+    std::cout << "*";
+  }
+}
+
+void runGame() {
+  growSnake(2, 3);
+  growSnake(3, 3);
+  growSnake(4, 3);
+  growSnake(5, 3);
+  printSnake();
+}
+
 int main() {
-  print_snake();
+  runGame();
   return 0;
 }
